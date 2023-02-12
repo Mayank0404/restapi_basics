6540723a0f4ccb85628948b0e339cbb0dffedb12
Mayank0404 <mayanksharma040404@gmail.com>
2023-02-12 18:33:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express =require('express')
+const app=express()
+
+const middleware=(req,res,next)=>{
+    console.log("hi this is middleware");
+    next()
+}
+
+app.use(middleware)
+
+
+app.post("/",(req,res)=>{
+    res.send("hi this is post request")
+})
+
+app.put("/",(req,res)=>{
+    res.send("hi this is put request")
+})
+app.delete("/",(req,res)=>{
+    res.send("hi this is delete request")
+})
+
+
+
+const port=3000
+app.listen(port,()=>console.log(`server s running at ${port}`))
+
